d0f787f82ee1a4bb6c190ade68ee49d7fab874a4
xuan <sakana9826@gmail.com>
2024-05-31 14:57:27 +0800

    按钮组件

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,32 @@
+<!--<template>-->
+<!--  <svg :class="svgClass" aria-hidden="true">-->
+<!--    <use :xlink:href="iconName" />-->
+<!--  </svg>-->
+<!--</template>-->
+
+<!--<script setup lang="ts">-->
+<!--import { computed } from 'vue'-->
+
+<!--const props = withDefaults(defineProps<{-->
+<!--  iconClass: string;-->
+<!--  className?: string;-->
+<!--}>(), { className: '' })-->
+
+<!--const iconName = computed(() => {-->
+<!--  return `#icon-${this.iconClass}`-->
+<!--})-->
+
+<!--const svgClass = computed(() => {-->
+<!--  if (this.className) {-->
+<!--    return 'svg-icon ' + this.className-->
+<!--  } else {-->
+<!--    return 'svg-icon'-->
+<!--  }-->
+<!--})-->
+<!--</script>-->
+
+<!--<style scoped>-->
+<!--.svg-icon {-->
+<!--  fill: currentColor;-->
+<!--}-->
+<!--</style>-->
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import '@/assets/css/main.css'
+import '@/assets/css/global.scss';
 import App from './App.vue'
 import router from './router'
 
