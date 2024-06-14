8f829581449f63f221991a57fa60807f1aa99f9f
xuan <sakana9826@gmail.com>
2024-06-14 08:38:14 +0800

    新增封面批量展示

---
+++ src/components/ExplicitSymbol.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg-icon name="explicit" :style="svgStyle"></svg-icon>
+</template>
+
+<script lang="ts">
+import SvgIcon from '@/components/SvgIcon.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ExplicitSymbol',
+  components: {
+    SvgIcon
+  },
+  props: {
+    size: {
+      type: Number,
+      default: 16
+    }
+  },
+  data() {
+    return {
+      svgStyle: {}
+    }
+  },
+  created() {
+    this.svgStyle = {
+      height: this.size + 'px',
+      width: this.size + 'px',
+      position: 'relative',
+      left: '-1px'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
