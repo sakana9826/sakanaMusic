5027867dc35d143b739244f937afbc5ad40e75b9
xuan <sakana9826@gmail.com>
2024-07-18 08:59:31 +0800

    精简代码

--- src/App.vue
+++ src/App.vue
@@ -9,27 +9,3 @@ import { RouterView } from 'vue-router'
     <RouterView />
   </main>
 </template>
-
-<style lang="scss" scoped>
-//main {
-//  position: fixed;
-//  top: 0;
-//  bottom: 0;
-//  right: 0;
-//  left: 0;
-//  overflow: auto;
-//  padding: 64px 10vw 96px 10vw;
-//  box-sizing: border-box;
-//  scrollbar-width: none; // firefox
-//}
-//
-//@media (max-width: 1336px) {
-//  main {
-//    padding: 64px 5vw 96px 5vw;
-//  }
-//}
-//
-//main::-webkit-scrollbar {
-//  width: 0px;
-//}
-</style>
